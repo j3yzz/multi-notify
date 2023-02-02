c142fea0872008bbdc93afe0b5c2e3d7a6ac40b2
amirreza <amir.rezakheradmand.2017@gmail.com>
2023-02-02 05:45:01 +0330

    add: add config file

---
+++ config/multi-notify.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    /*
+    |------------------------------------
+    | Default SMS and E-mail Drivers
+    |------------------------------------
+    |
+    | This parameter selects the appropriate gateway from the options below.
+    | You can switch driver using env variable.
+    |
+     */
+    'sms_default_driver' => env('NOTIFY_SMS_DEFAULT_DRIVER', 'testSms'),
+    'mail_default_driver' => env('NOTIFY_MAIL_DEFAULT_DRIVER', 'testMail'),
+
+    /*
+    |------------------------------------
+    | Active Method Status
+    |------------------------------------
+    |
+    | These values determine whether the methods are active or inactive,
+    | as they represent a boolean value.
+    | You can switch active or inactive method using env variable.
+    |
+     */
+    'sms_active' => env('SMS_ACTIVE', true),
+    'mail_active' => env('MAIL_ACTIVE', true),
+
+    /*
+    |------------------------------------
+    | List of drivers
+    |------------------------------------
+    |
+    |
+     */
+    'drivers' => [
+        'sms' => [
+
+        ],
+        'mail' => [
+
+        ],
+    ],
+
+    /*
+    |------------------------------------
+    | Class maps
+    |------------------------------------
+    |
+    |
+     */
+    'map' => [
+        'sms' => [
+
+        ],
+        'mail' => [
+
+        ],
+    ]
+];
